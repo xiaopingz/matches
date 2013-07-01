111285c1d3cab349ffb0423ebfd96f28721542ab
xiaoping zhou <xiao1119888@163.com>
2013-07-01 19:52:30 +0800

    fffffffffffffffffffffffffffff

--- matches/maleRecord.h
+++ matches/maleRecord.h
@@ -13,7 +13,8 @@ public:
 	male_data	getElementById(int id);
 	void	addMaleRecord(int id, int wealth, int look, int charactor, int rw, int rl, int rc);
 	void	addDegree(FemaleRecord& fr);	//针对每一个男用户，遍历女用户表，计算满意度。生成男性记录总表（未排序）
-	void	vote(FemaleRecord& fr);						//遍历男用户表，对女性用户进行一次投票           
+	void	vote(FemaleRecord& fr);						//遍历男用户表，对女性用户进行一次投票   
+	void	deleteCandidate(int idMale,int idFemale);	//给出男用户与其投过票的女用户的id,删除对应的数据。
 	
 
 protected:
